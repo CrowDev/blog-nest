4f011707dd74823ddac658c2e9338c55cceda1ee
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-11-09 11:10:21 -0300

    inject category repository to service

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CategoryEntity } from '../entities/Category.entity';
 import { CategoriesController } from './controller/categories.controller';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([CategoryEntity])],
   controllers: [CategoriesController],
 })
 export class CategoriesModule {}
---
+++ src/categories/services/categories.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CategoryEntity } from 'src/entities/Category.entity';
+import { Repository } from 'typeorm';
+import { CreateCategoryDto } from '../dto/CategoriesDto.dto';
+
+@Injectable()
+export class CategoriesService {
+  constructor(
+    @InjectRepository(CategoryEntity)
+    private categoryEntityRepository: Repository<CategoryEntity>,
+  ) {}
+
+  async create(createCategoryDto: CreateCategoryDto) {
+    const newCategory = new CategoryEntity();
+    newCategory.name = createCategoryDto.name;
+    const result = await this.categoryEntityRepository.save(newCategory);
+    return result;
+  }
+}
