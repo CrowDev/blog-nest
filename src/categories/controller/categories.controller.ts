1676547a86e2545c16270d4967235cfb836073b2
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-11-09 10:40:01 -0300

    added params and body to controller methods

--- src/categories/controller/categories.controller.ts
+++ src/categories/controller/categories.controller.ts
@@ -7,6 +7,7 @@ import {
   Body,
   Param,
 } from '@nestjs/common';
+import { CategoryDto, CreateCategoryDto } from '../dto/CategoriesDto.dto';
 
 @Controller('categories')
 export class CategoriesController {
@@ -16,22 +17,22 @@ export class CategoriesController {
   }
 
   @Get(':id')
-  findOne(): string {
+  findOne(@Param() id: string): string {
     return 'This action returns a #${id} category';
   }
 
   @Post('create')
-  create(): string {
+  create(@Body() categoryDto: CreateCategoryDto): string {
     return 'This action adds a new category';
   }
 
   @Put(':id')
-  update(): string {
+  update(@Body() categoryDto: CategoryDto): string {
     return 'This action updates a #${id} category';
   }
 
   @Delete(':id')
-  remove(): string {
+  remove(@Param() id: string): string {
     return 'This action removes a #${id} category';
   }
 }
