9370674e9e6b1886faaf6a09d239e05e87306ae0
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-11-09 19:05:55 -0300

    sending blog dto to save relation

--- src/comments/dto/CommentsDto.dto.ts
+++ src/comments/dto/CommentsDto.dto.ts
@@ -1,9 +1,13 @@
+import { BlogDto } from '@/blogs/dto/BlogsDto.dto';
+
 export class CommentDto {
   id: number;
   comment: string;
   publicationDate: Date;
+  blog: BlogDto;
 }
 
 export class CreateComentDto {
   comment: string;
+  blog: BlogDto;
 }
