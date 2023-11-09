b1780edb0806a49be857394469574dd090cbbbe6
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-11-09 11:16:43 -0300

    added category service as provider

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { CategoryEntity } from '../entities/Category.entity';
 import { CategoriesController } from './controller/categories.controller';
+import { CategoriesService } from './services/categories.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CategoryEntity])],
   controllers: [CategoriesController],
+  providers: [CategoriesService],
 })
 export class CategoriesModule {}
