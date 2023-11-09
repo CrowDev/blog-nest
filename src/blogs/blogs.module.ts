291bdcaea55e478f63836c76b952af9638782f10
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-11-09 09:23:02 -0300

    added typeorm module to handle Repository

--- src/blogs/blogs.module.ts
+++ src/blogs/blogs.module.ts
@@ -1,7 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BlogEntity } from '../entities/Blog.entity';
 import { BlogsController } from './controller/blogs.controller';
+import { BlogsService } from './services/blogs.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([BlogEntity])],
   controllers: [BlogsController],
+  providers: [BlogsService],
 })
 export class BlogsModule {}
