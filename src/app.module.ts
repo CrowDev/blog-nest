e1c6526338d72f305aa8a78ae5441d7849d57f10
CrowDev <h.aguilarvalenzuela@gmail.com>
2023-11-08 21:40:33 -0300

    added sqlite config and blog module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BlogsModule } from './blogs/blogs.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: 'db.sqlite',
+      entities: [],
+      synchronize: true,
+    }),
+    BlogsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
